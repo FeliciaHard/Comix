f440c94da66b69d8534aa98899f4d0cfdcf45641
FeliciaHard <feliciahardy505@gmail.com>
2024-10-21 08:25:45 +0800

    Add files via upload

--- main/php/pop-ups/pop-ups.php
+++ main/php/pop-ups/pop-ups.php
@@ -11,7 +11,7 @@
                 <use xlink:href="#exclamation-triangle-fill"/>
             </svg>';
 
-        echo '<div>Check your <strong>no. ic</strong> & <strong>password </strong>again.</div>
+        echo '<div>Check your <strong>username</strong> & <strong>password </strong>again.</div>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>';
     }
@@ -20,7 +20,7 @@
         $wrong_duplicatekey = $_GET['DUPLICATE-KEY'];
 
         echo '
-        <div class="alert alert-warning alert-dismissible fade show d-flex align-items-center" role="alert">';  
+        <div class="alert alert-warning alert-dismissible fade show d-flex align-items-center rounded-pill" role="alert">';  
 
         echo '
             <svg class="bi flex-shrink-0 me-2" width="24" height="30" role="img" aria-label="Danger:">
@@ -28,6 +28,54 @@
             </svg>';
 
         echo '<div>Their maybe a <strong>duplicate key</strong>. Please check again.</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+    }
+
+    if (isset($_GET['SUCCESS'])) {
+        $wrong_duplicatekey = $_GET['SUCCESS'];
+
+        echo '
+        <div class="alert alert-success alert-dismissible fade show d-flex align-items-center rounded-pill" role="alert">';  
+
+        echo '
+            <svg class="bi flex-shrink-0 me-2" width="24" height="30" role="img" aria-label="Danger:">
+                <use xlink:href="#exclamation-triangle-fill"/>
+            </svg>';
+
+        echo '<div>New Comix Created <strong>Successfully!</strong></div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+    }
+
+    if (isset($_GET['UPDATED'])) {
+        $wrong_duplicatekey = $_GET['UPDATED'];
+
+        echo '
+        <div class="alert alert-success alert-dismissible fade show d-flex align-items-center rounded-pill" role="alert">';  
+
+        echo '
+            <svg class="bi flex-shrink-0 me-2" width="24" height="30" role="img" aria-label="Danger:">
+                <use xlink:href="#exclamation-triangle-fill"/>
+            </svg>';
+
+        echo '<div>A Data Has Been Updated <strong>Successfully!</strong></div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+    }
+
+    if (isset($_GET['DELETED'])) {
+        $wrong_duplicatekey = $_GET['DELETED'];
+
+        echo '
+        <div class="alert alert-success alert-dismissible fade show d-flex align-items-center rounded-pill" role="alert">';  
+
+        echo '
+            <svg class="bi flex-shrink-0 me-2" width="24" height="30" role="img" aria-label="Danger:">
+                <use xlink:href="#exclamation-triangle-fill"/>
+            </svg>';
+
+        echo '<div>A Data Has Been Deleted <strong>Successfully!</strong></div>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>';
     }
